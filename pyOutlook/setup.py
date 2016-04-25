96d97729d4acfa5b560b6fbc5afdbbd1a5735d39
Jens Astrup <jensaiden@gmail.com>
2016-04-25 01:42:44 -0400

    Setup update for PKG info + landscape

--- pyOutlook/setup.py
+++ pyOutlook/setup.py
@@ -16,10 +16,12 @@ setuptools.setup(
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Communications :: Email :: Email Clients (MUA)',
+        'Topic :: Office/Business',
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English'
     ]
 )
