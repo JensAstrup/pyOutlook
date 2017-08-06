c13a2e9163dc463411a06e6ce2e1a5556150afc0
Jens Astrup <jensaiden@gmail.com>
2017-08-05 20:19:58 -0400

    Additional tests for #6

--- tests/account_tests.py
+++ tests/account_tests.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from datetime import datetime
+
 from pyOutlook import *
 
 
@@ -25,3 +27,25 @@ class TestAccount(TestCase):
 
         # There should be nothing left in the headers
         self.assertFalse(bool(headers))
+
+    def test_auto_reply_start_date_must_be_datetime(self):
+        account = OutlookAccount('test')
+
+        with self.assertRaisesRegexp(ValueError, 'Start and End must both either be None or datetimes'):
+            account.set_auto_reply('test message', start='not a date', end=datetime.today())
+
+    def test_auto_reply_end_date_must_be_datetime(self):
+        account = OutlookAccount('test')
+
+        with self.assertRaisesRegexp(ValueError, 'Start and End must both either be None or datetimes'):
+            account.set_auto_reply('test message', start=datetime.today(), end='not a date')
+
+    def test_auto_reply_start_and_end_date_required(self):
+        """ Test that a start date and end date must be given together """
+        account = OutlookAccount('123')
+
+        with self.assertRaisesRegexp(ValueError, "Start and End not must both either be None or datetimes"):
+            account.set_auto_reply('message', start=datetime.today())
+
+        with self.assertRaisesRegexp(ValueError, "Start and End not must both either be None or datetimes"):
+            account.set_auto_reply('message', end=datetime.today())
