f8a2bdda6f0689be5cc4391fe82af1f7fd618df4
Jens Astrup <jens@wellthy.com>
2018-07-28 07:28:08 -0400

    Increment version

--- pyOutlook/__init__.py
+++ pyOutlook/__init__.py
@@ -1,5 +1,5 @@
 from .core import *
 
 __all__ = ['OutlookAccount', 'Message', 'Contact', 'Folder', 'Attachment']
-__version__ = '4.2.1'
-__release__ = '4.2.1'
+__version__ = '4.2.2'
+__release__ = '4.2.2'
