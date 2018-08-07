736cf3da1243f4f095efc0e390b8b82f0c03413d
Jens Astrup <jens@wellthy.com>
2018-08-07 11:36:46 -0400

    Add additional tests to cover #32

--- tests/account_tests.py
+++ tests/account_tests.py
@@ -1,6 +1,6 @@
-from unittest import TestCase
-
 from datetime import datetime
+import mock
+from unittest import TestCase
 
 from pyOutlook import *
 
@@ -49,3 +49,25 @@ class TestAccount(TestCase):
 
         with self.assertRaisesRegexp(ValueError, "Start and End not must both either be None or datetimes"):
             account.set_auto_reply('message', end=datetime.today())
+
+    @mock.patch.object(Message, '__init__')
+    def test_new_email(self, message_init):
+        message_init.return_value = None
+        account = OutlookAccount('token')
+        body = 'Test Body'
+        subject = 'My Subject'
+        to = ['some_dude@email.com']
+        account.new_email(body, subject, to)
+        message_init.assert_called_once_with(account, body, subject, to)
+
+    @mock.patch.object(Message, 'send')
+    @mock.patch.object(Message, '__init__')
+    def test_send_email(self, message_init, send):
+        message_init.return_value = None
+        account = OutlookAccount('token')
+        body = 'Test Body'
+        subject = 'Test Subject'
+        to = ['dude@email.com']
+        account.send_email(body, subject, to)
+        message_init.assert_called_once_with(account, body, subject, to, bcc=None, cc=None, sender=None)
+        send.assert_called_once()
