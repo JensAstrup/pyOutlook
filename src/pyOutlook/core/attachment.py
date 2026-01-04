ba5ab10a1858d0a7f0dcb66bc5d98d003e6647cd
Jens Astrup <jensaiden@gmail.com>
2026-01-03 19:50:31 -0500

    Update Attachment class to be more API-friendly and add type hints

--- src/pyOutlook/core/attachment.py
+++ src/pyOutlook/core/attachment.py
@@ -1,13 +1,15 @@
+from typing import Any, Iterator
 from base64 import b64decode
 
 from datetime import datetime
-from dateutil import parser
 
 __all__ = ['Attachment']
 
 
 class Attachment(object):
-    def __init__(self, name: str, content: str, outlook_id: str | None = None, size: int | None = None, last_modified: datetime | None = None, content_type: str | None = None):
+    def __init__(self, name: str, content: str, outlook_id: str | None = None, 
+                size: int | None = None, last_modified: datetime | None = None, 
+                content_type: str | None = None):
         self.name = name
         self._content = content
         self.bytes = b64decode(content)
@@ -16,19 +18,11 @@ class Attachment(object):
         self.last_modified = last_modified
         self.content_type = content_type
 
-    @classmethod
-    def json_to_attachment(cls, account, api_json):
-        '''Backward compatibility: delegates to MessageService.'''
-        from pyOutlook.services.message import MessageService
-        return MessageService._json_to_attachment(account, api_json)
-
-    @classmethod
-    def json_to_attachments(cls, account, api_json):
-        '''Backward compatibility: delegates to MessageService.'''
-        from pyOutlook.services.message import MessageService
-        return MessageService._json_to_attachments(account, api_json)
-
-    def api_representation(self):
-        """ Used for uploading attachments - less information is required than what we receive from the API """
-        return {'@odata.type': '#Microsoft.OutlookServices.FileAttachment', 'Name': self.name,
-                'ContentBytes': self._content}
+    def __iter__(self) -> Iterator[tuple[str, Any]]:
+        """ Used for uploading attachments - allows dict(attachment) to work.
+        Less information is required than what we receive from the API.
+        """
+        yield '@odata.type', '#microsoft.graph.fileAttachment'
+        yield 'name', self.name
+        yield 'contentBytes', self._content
+        yield 'contentType', self.content_type
